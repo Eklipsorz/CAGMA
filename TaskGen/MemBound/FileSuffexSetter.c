42abe73083e6098d300493830b94cd3968b2f9b6
GCCode <sslouis25@icloud.com>
2017-09-21 14:36:10 +0800

    add comment in FileSuffexSetter.c

--- TaskGen/MemBound/FileSuffexSetter.c
+++ TaskGen/MemBound/FileSuffexSetter.c
@@ -1,19 +1,25 @@
 #include "FixedAccMem.h"
 
+/* This program helps user modify the suffex of a file storing each data from
+ * each memory-bound task. You first run this program with a parameter which 
+ * can be the suffex of the file 
+ */
 
-
-void begin(char *input,int len)
+/* This function modify the suffex via writing /proc/fsuffexSetter */
+void SuffexSetter(char *input,int len)
 {
 	
 	int configfd;
-	
-	configfd = open("/proc/enabler", O_RDWR);	
+
+	/* require for accessing /proc/fsuffexSetter */
+	configfd = open("/proc/fsuffexSetter", O_RDWR);	
 	
 	if(configfd < 0) {
 		perror("open");
 		return;
 	}
 
+	/* write new suffex into /proc/fsuffexSetter */
 	write(configfd,input,len);
 	close(configfd);
 
@@ -24,17 +30,22 @@ int main(int argc,char *argv[])
 	char *fileNum = NULL;
 	int len = 0;
 
+
+	/* check whether the number of the inputted arguments is 2 */
 	if (argc < 2)
 	{
 		printf("u cannot specify fileNum\n");
 		return 0;
 	}
-	
+
+	/* allocate memory to a string */
+	/* then the Guest OS can give the argument into the string */
 	len = strlen(argv[1]) + 1;
 	fileNum = (char *) calloc(sizeof(char),len);	
 	strcpy(fileNum,argv[1]);
-	
-	begin(fileNum,len);
+
+	/* modify the suffex of the file storing each data from memory-bound task */
+	SuffexSetter(fileNum,len);
 
 	return 0;
 }
